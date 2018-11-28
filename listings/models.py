e7b4f7d98f0a1c124b1dbc97aa9cd57174fb70bf
Lenny <ldrnuy@gmail.com>
2018-11-27 22:35:29 -0300

    migrations, admin page customization, etc

--- listings/models.py
+++ listings/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from datetime import datetime
 from realtors.models import Realtor
 
-class Listings(models.Model):
+class Listing(models.Model):
 	realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
 	title = models.CharField(max_length=200)
 	address = models.CharField(max_length=200)
 	city = models.CharField(max_length=100)
 	state = models.CharField(max_length=100)
 	zipcode = models.CharField(max_length=20)
+	price = models.IntegerField()
 	description = models.TextField(blank=True)
 	bedrooms = models.IntegerField()
 	bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
